c62e5277ddb6cf08985d636e761726379e8523db
Allyson <jandreyallyson@gmail.com>
2023-05-10 19:48:50 -0300

    feat - imagem de mais no botão

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,12 +1,16 @@
 import styles from './index.module.css'
+import plus from '../../assets/plus.svg'
 
 export const Content = () => {
     return (
         <section className={styles.section_container}>
             <main>
                 <article className={styles.input_container}>
-                    <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa"/>
-                    <button className={styles.button}>Criar</button>
+                    <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa" />
+                    <button className={styles.button}>Criar
+                        <img
+                            src={plus}
+                            alt="Ícone de mais" /></button>
                 </article>
             </main>
         </section>

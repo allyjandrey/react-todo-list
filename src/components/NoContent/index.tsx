3d7dc012af56ad83ec3778d1094431a89cb4ef7d
Allyson <jandreyallyson@gmail.com>
2023-05-10 20:13:42 -0300

    feat - tela sem conteúdo

---
+++ src/components/NoContent/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css'
+
+export const NoContent = () => {
+    return (
+        <section>
+            <img src="" alt="" />
+            <p>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                Crie tarefas e organize seus itens
+            </p>
+        </section>
+    )
+}

65cd03a08ce565994ef0cebf0687990ed35dcf8f
Allyson <jandreyallyson@gmail.com>
2023-05-11 19:56:21 -0300

    feat - checkbox arrow

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -6,7 +6,10 @@ export const TodoList = () => {
         <section className={styles.section_container}>
             <article className={styles.content_container}>
                 <input type="checkbox" />
-                <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam velit sed dignissimos cupiditate? Ducimus, dignissimos! Minus maiores laborum facere velit nihil hic delectus cumque sed sunt, quaerat neque tempora corrupti!</p>
+                <p className={styles.text}>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. Ex repudiandae necessitatibus itaque magnam nobis
+                  rerum repellendus provident eos incidunt, deserunt aut perspiciatis.
+                   Accusamus reiciendis error odio quod nemo optio laborum?</p>
                 <img src={trash} alt="Ícone de lixeira" />
             </article>
         </section>

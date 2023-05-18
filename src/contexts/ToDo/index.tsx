3bf6e974857e4e19388cbd4ad05c6544b260c826
Allyson <jandreyallyson@gmail.com>
2023-05-17 21:07:56 -0300

    criação do contexto

--- src/contexts/ToDo/index.tsx
+++ src/contexts/ToDo/index.tsx
@@ -1,11 +1,33 @@
-import { createContext } from "react";
+import React, { Children, createContext, useState } from "react";
 import { Task } from "../../models/Task";
+
+type ToDoContextProviderProps = {
+    children: React.ReactNode
+}
 
 type ToDoContextProps = {
     taskListState: Task[],
-    setTaskListState: React.Dispatch<React.SetStateAction<Task[]>>
+    setTaskListState: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-const ToDoContext = createContext({})
+const DEFAULT_VALUES = {
+    taskListState: [],
+    setTaskListState: () => [{}]
+}
+
+const ToDoContext = createContext<ToDoContextProps>(DEFAULT_VALUES);
+
+const ToDoContextProvider = ({ children }: ToDoContextProviderProps) => {
+    const [taskListState, setTaskListState] = useState<Task[]>([])
+    return (<ToDoContext.Provider value={{
+        taskListState,
+        setTaskListState
+    }}>
+        {children}
+    </ToDoContext.Provider>
+    );
+}
+
+export { ToDoContextProvider }
 
 export default ToDoContext;

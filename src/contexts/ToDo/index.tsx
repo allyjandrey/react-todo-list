becc923d22978c31537346c3b331f4a2cc02d8ac
Allyson <jandreyallyson@gmail.com>
2023-05-17 21:08:10 -0300

    criação do contexto

--- src/contexts/ToDo/index.tsx
+++ src/contexts/ToDo/index.tsx
@@ -1,4 +1,4 @@
-import React, { Children, createContext, useState } from "react";
+import React, {createContext, useState } from "react";
 import { Task } from "../../models/Task";
 
 type ToDoContextProviderProps = {

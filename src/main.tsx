918990be57d1f7e077c57e47a78a8b7f9b79a475
Allyson <jandreyallyson@gmail.com>
2023-05-23 20:23:10 -0300

    feat - implementando a rota

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './global.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
